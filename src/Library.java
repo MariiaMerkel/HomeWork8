3037a72df38a531561a4938f77c5ebe7535b7abb
Mariia <mariia_merkel@mail.ru>
2024-01-16 10:24:52 +0300

    Выполнила задания повышенной сложности

---
+++ src/Library.java
@@ -0,0 +1,50 @@
+public class Library {
+    private Book[] books;
+
+    public Library(int amount) {
+        this.books = new Book[amount];
+    }
+
+    public void addBook(Book book) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = book;
+                return;
+            }
+        }
+    }
+
+    public void printBooks() {
+        for (Book book : books) {
+            if(book != null) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public void printBookByTitle(String title) {
+        final Book book = this.findBookByTitle(title);
+        if (book != null) {
+            System.out.printf("\"%s\" была опубликованна в %d году.%n", book.getTitle(), book.getPublicationYear());
+        } else {
+            System.out.println("Такой книги нет");
+        }
+    }
+
+    private Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void changPublicationYear(String title, int year) {
+        final Book book = this.findBookByTitle(title);
+        if (book != null) {
+            book.setPublicationYear(year);
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-import java.util.Random;
-
 public class Main {
+
     public static void main(String[] args) {
 
+        Author walterScott = new Author("Вальтер", "Скотт");
+        Author hansChristianAndersen = new Author("Ханс Кристиан", "Андерсен");
+        Author charlesDickens = new Author("Чарльз", "Диккенс");
+
+        Book snowQueen = new Book("Снежная Королева", hansChristianAndersen, 1844);
+        Book waverley = new Book("Уэверли или Шестьдесят лет назад", walterScott, 1814);
+        Book adventuresOfOliverTwist = new Book("Приключения Оливера Твиста", charlesDickens, 1837);
+
+        Library library = new Library(3);
+        library.addBook(snowQueen);
+        library.addBook(waverley);
+        library.addBook(adventuresOfOliverTwist);
+
+        library.printBooks();
+
+        library.printBookByTitle("Снежная Королева");
+        library.changPublicationYear("Приключения Оливера Твиста", 1839);
+        library.printBookByTitle("Приключения Оливера Твиста");
     }
+
+
 }
